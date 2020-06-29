aebc3b0c1e2c9695f4725fd843c199173e273eb7
Daniel Serna <Daniel Serna@DESKTOP-7JHTDAT>
2020-06-28 21:15:44 -0500

    Daniel-Genere las clases de la parte Azul

---
+++ src/modelo/Vendedor.java
@@ -0,0 +1,29 @@
+package modelo;
+
+import java.io.File;
+import java.sql.Date;
+
+/**
+ *
+ * @author Daniel Serna
+ */
+public class Vendedor extends Persona{
+    //Declaracion de Variables
+    private String turno;
+    private String horaEntrada;
+    private String horaSalida;
+    
+    //Metodos
+    public File generaTicketVenta(){
+        
+        return null;
+    }
+    public Date registraHoraEntrada(){
+        
+        return null;
+    }
+    public boolean revisaBonoPuntualidad(){
+        
+        return false;
+    }
+}
