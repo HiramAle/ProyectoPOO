485136994b098176ff6d447118ba35d3e3f2d546
magic <magic@OmenHiram>
2020-07-07 22:13:11 -0500

    Logica Completa

--- src/modelo/Compra.java
+++ src/modelo/Compra.java
@@ -5,17 +5,39 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Compra {
+    private int idCompra;
     private Date fecha;
     private double monto;
     private Proveedor prov;
+    private int cantidad;
     private ArrayList <Producto> pds = new ArrayList <Producto> () ;
 
-    public Compra(Date fecha, double monto, Proveedor prov, ArrayList <Producto> pds) {
+    public Compra(int idCompra,int cantidad,Date fecha, double monto, Proveedor prov, ArrayList <Producto> pds) {
+        this.idCompra = idCompra;
         this.fecha = fecha;
         this.monto = monto;
         this.prov = prov;
         this.pds = pds;
+        this.cantidad = cantidad;
     }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
 
     public Date getFecha() {
         return fecha;
